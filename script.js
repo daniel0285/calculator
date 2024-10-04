0a0b3eaaf290e275aedf0f4787fab829e412b162
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2024-10-04 10:26:53 +0800

    refactor: fix calculator logic
    - added a message if user tries to divide 0
    - calculate the number if an operator is pressed
    instead of equals
    - changed parseInt to parseFloat to display numbers
    with decimal

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ const MATH_OPERATORS = ["add", "subtract", "multiply", "divide"];
 
 calculatorButtons.addEventListener("click", (event) => {
   const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  const deleteAndClear = ["delete", "clear"];
+  const DELETE_AND_CLEAR = ["delete", "clear"];
 
   if (event.target.id !== "calculator-btn-container") {
     if (DIGITS.includes(event.target.innerText)) {
       insertDigits(event.target.id);
-      // console.log(event.target.innerText);
     } else if (MATH_OPERATORS.includes(event.target.id)) {
       if (secondNumber.length == 0) {
         assignOperator(event.target.id, event.target.innerText);
-        // console.log(event.target.innerText);
+      } else if (!(firstNumber.length == 0 && secondNumber.length == 0)) {
+        calculateUserInput(firstNumber, currentOperator, secondNumber);
+        assignOperator(event.target.id, event.target.innerText);
       }
-    } else if (deleteAndClear.includes(event.target.id)) {
+    } else if (DELETE_AND_CLEAR.includes(event.target.id)) {
       console.log(event.target.innerText);
     } else if (event.target.id === "equal") {
       if (secondNumber.length !== 0 && currentOperator.name !== undefined) {
@@ -41,32 +42,32 @@ function insertDigits(userInput) {
 function updateCalculatorDisplay() {
   const display = [];
   const currentCalculatorDisplay = display
-    .concat(firstNumber, currentOperator.symbol, secondNumber)
+    .concat(firstNumber, " ", currentOperator.symbol, " ", secondNumber)
     .join("");
   calculatorDisplay.innerText = currentCalculatorDisplay;
 }
 
-function checkInputLimitations(currentNumber, userInput) {
-  if (
-    (currentNumber[0] === "0" &&
-      currentNumber[1] !== "." &&
-      userInput !== ".") ||
-    (firstNumber.includes(".") && userInput === ".")
-  ) {
+function checkInputLimitations(arrayNumber, userInput) {
+  if (firstNumber.includes(".") && userInput == ".") {
     doNothing;
   } else {
-    currentNumber.push(userInput);
-    updateCalculatorDisplay();
-    console.log(`currentNumber`);
+    if (arrayNumber[0] == "0" && arrayNumber[1] != ".") {
+      arrayNumber.splice(0, arrayNumber.length, userInput);
+    } else if (arrayNumber[0] == ".") {
+      arrayNumber.unshift("0");
+      arrayNumber.push(userInput);
+    } else {
+      arrayNumber.push(userInput);
+    }
   }
+  updateCalculatorDisplay();
 }
 
 function assignOperator(name, symbol) {
-  if (firstNumber.length !== 0) {
+  if (firstNumber.length != 0) {
     currentOperator.name = name;
     currentOperator.symbol = symbol;
     updateCalculatorDisplay();
-    console.log(`new operator: ${currentOperator.name}`);
   }
 }
 
@@ -79,8 +80,16 @@ function calculateUserInput(firstInput, operator, secondInput) {
   } else if (operator.name === "multiply") {
     result = convertToInteger(firstInput) * convertToInteger(secondInput);
   } else if (operator.name === "divide") {
-    result = convertToInteger(firstInput) / convertToInteger(secondInput);
+    if ((firstInput == 0 && secondInput == 0) || secondInput == 0) {
+      result = "Division by zero is undefined";
+    } else {
+      result = convertToInteger(firstInput) / convertToInteger(secondInput);
+    }
   }
+  displayResult(result);
+}
+
+function displayResult(result) {
   calculatorDisplay.innerText = result;
   firstNumber.splice(0, firstNumber.length, result);
   secondNumber.length = 0;
@@ -88,7 +97,7 @@ function calculateUserInput(firstInput, operator, secondInput) {
 }
 
 function convertToInteger(arr) {
-  return parseInt(arr.join(""));
+  return parseFloat(arr.join(""));
 }
 
 function doNothing() {
