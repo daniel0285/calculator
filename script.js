3f1b07a69f8d10ded172e0c93abc647ea8a4270e
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2024-10-06 20:16:51 +0800

    refactor: try to make it more readable

--- script.js
+++ script.js
@@ -1,82 +1,67 @@
+// DOM Elements
 const calculatorDisplayInput = document.getElementById("calculator-input");
 const calculatorResultDisplay = document.getElementById("calculator-result");
 const calculatorButtons = document.getElementById("calculator-btn-container");
+
+// Constants
+const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const ACTIONS = ["delete", "clear", "equal"];
+const OPERATORS = ["add", "subtract", "multiply", "divide"];
 
 let firstNumber = [];
 let currentOperator = {};
 let secondNumber = [];
 let result = 0;
 
-calculatorButtons.addEventListener("click", (event) => {
-  if (event.target.id !== "calculator-btn-container") {
-    calculatorHandler(event);
-  }
-});
+// Event Listeners
+calculatorButtons.addEventListener("click", (e) =>
+  e.target.id !== "calculator-btn-container" ? calculatorButtonHandler(e) : null
+);
 
-window.addEventListener("keypress", (e) => {
-  if (e.key == 8) {
-    alert("8");
-  }
-});
+function calculatorButtonHandler(e) {
+  const eventText = e.target.innerText;
+  const eventId = e.target.id;
 
-function calculatorHandler(event) {
-  const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-  const DELETE_AND_CLEAR = ["delete", "clear"];
-  const MATH_OPERATORS = ["add", "subtract", "multiply", "divide"];
+  if (DIGITS.includes(eventId)) handleDigitInput(eventId);
+  if (OPERATORS.includes(eventId)) handleOperatorInput(eventId, eventText);
+  if (ACTIONS.includes(eventId)) handleActionInput(eventId);
+}
 
-  const eventText = event.target.innerText;
-  const eventId = event.target.id;
-
-  if (DIGITS.includes(eventText)) {
-    insertDigits(eventId);
-  } else if (MATH_OPERATORS.includes(eventId)) {
-    if (secondNumber.length == 0) {
-      assignOperator(eventId, eventText);
-    } else if (!(firstNumber.length == 0 && secondNumber.length == 0)) {
-      assignOperator(eventId, eventText);
-    }
-  } else if (DELETE_AND_CLEAR.includes(eventId)) {
-    deleteAndClearDigits(eventId);
-  } else if (eventId === "equal") {
-    if (secondNumber.length !== 0 && currentOperator.name !== undefined) {
-      calculateUserInput(firstNumber, currentOperator, secondNumber);
-    }
+function handleDigitInput(userInput) {
+  if (firstNumber.length > 0 && currentOperator.name) {
+    updateNumberInput(secondNumber, userInput);
+  } else {
+    updateNumberInput(firstNumber, userInput);
   }
 }
 
-function insertDigits(userInput) {
-  if (firstNumber.length !== 0 && currentOperator.name !== undefined) {
-    checkInputBehavior(secondNumber, userInput);
-  } else {
-    checkInputBehavior(firstNumber, userInput);
+function handleOperatorInput(name, symbol) {
+  if (secondNumber.length == 0) {
+    updateOperator(name, symbol);
+  } else if (!isArrayEmpty(firstNumber) && !isArrayEmpty(secondNumber)) {
+    updateOperator(name, symbol);
+    calculateUserInput();
   }
 }
 
-function deleteAndClearDigits(userInput) {
-  if (userInput === "delete") {
-    backSpace();
-  } else if (userInput === "clear") {
-    clearCalculator();
-  } else {
-    alert("invalid input");
-  }
+function handleActionInput(userInput) {
+  if (userInput === "delete") removeLastInput();
+  if (userInput === "clear") clearCalculator();
+  if (userInput === "equal") calculateUserInput();
 }
 
 function clearCalculator() {
   firstNumber = [];
   currentOperator = {};
   secondNumber = [];
-  calculatorDisplayInput.textContent = "";
+  calculatorDisplayInput.textContent = "0";
   calculatorResultDisplay.textContent = "0";
 }
 
-function backSpace() {
-  if (isThisArrayEmpty(secondNumber) && currentOperator.name == undefined) {
+function removeLastInput() {
+  if (isArrayEmpty(secondNumber) && !currentOperator.name) {
     firstNumber.pop();
-  } else if (
-    isThisArrayEmpty(secondNumber) &&
-    currentOperator.name !== undefined
-  ) {
+  } else if (isArrayEmpty(secondNumber) && currentOperator.name) {
     currentOperator = {};
   } else {
     secondNumber.pop();
@@ -86,27 +71,22 @@ function backSpace() {
 
 function updateInputDisplay() {
   const currentCalculatorDisplay = []
-    .concat(firstNumber, " ", currentOperator.symbol, " ", secondNumber)
+    .concat(...firstNumber, " ", currentOperator.symbol, " ", secondNumber)
     .join("");
-
-  if (isThisArrayEmpty(firstNumber)) {
-    calculatorDisplayInput.innerText = "0";
-  } else {
-    calculatorDisplayInput.innerText = currentCalculatorDisplay;
-  }
+  calculatorDisplayInput.innerText = currentCalculatorDisplay || 0;
 }
 
 function updateResultDisplay(result) {
   firstNumber = result;
   secondNumber = [];
-  calculatorResultDisplay.innerText = "= " + result.join("");
+  calculatorResultDisplay.innerText = `= ${result.join("")}`;
 }
 
-function checkInputBehavior(arrayNumber, userInput) {
+function updateNumberInput(arrayNumber, userInput) {
   if (!(arrayNumber.includes(".") && userInput == ".")) {
     if (arrayNumber[0] == "0" && arrayNumber[1] != ".") {
       arrayNumber.splice(0, arrayNumber.length, userInput);
-    } else if (isThisArrayEmpty(arrayNumber) && userInput == ".") {
+    } else if (isArrayEmpty(arrayNumber) && userInput == ".") {
       arrayNumber.push("0", userInput);
     } else {
       arrayNumber.push(userInput);
@@ -115,45 +95,51 @@ function checkInputBehavior(arrayNumber, userInput) {
   updateInputDisplay();
 }
 
-function assignOperator(name, symbol) {
-  if (firstNumber.length != 0) {
-    currentOperator.name = name;
-    currentOperator.symbol = symbol;
+function updateOperator(name, symbol) {
+  if (firstNumber.length > 0) {
+    currentOperator = { name, symbol };
     updateInputDisplay();
   }
 }
 
-function calculateUserInput(firstInput, operator, secondInput) {
-  const num1 = convertToNumber(firstInput);
-  const num2 = convertToNumber(secondInput);
-  if (operator.name === "add") {
-    result = num1 + num2;
-  } else if (operator.name === "subtract") {
-    result = num1 - num2;
-  } else if (operator.name === "multiply") {
-    result = num1 * num2;
-  } else if (operator.name === "divide") {
-    if ((num1 == 0 && num2 == 0) || num2 == 0) {
-      alert("Division by zero is undefined");
-    } else {
-      result = num1 / num2;
+function calculateUserInput() {
+  if (!isArrayEmpty(firstNumber) && !isArrayEmpty(secondNumber)) {
+    const num1 = convertToNumber(firstNumber);
+    const num2 = convertToNumber(secondNumber);
+    switch (currentOperator.name) {
+      case "add":
+        result = num1 + num2;
+        break;
+      case "subtract":
+        result = num1 + num2;
+        break;
+      case "multiply":
+        result = num1 * num2;
+        break;
+      case "divide":
+        if (num2 === 0) {
+          alert("Division by zero is undefined");
+          return;
+        }
+        result = num1 / num2;
+        break;
     }
+    updateResultDisplay(convertToArray(formatResult(result)));
   }
-  updateResultDisplay(convertToArray(checkIfNumberHasDecimal(result)));
 }
 
 function convertToNumber(arr) {
-  return parseFloat(arr.join(""));
+  return isArrayEmpty(arr) ? 0 : parseFloat(arr.join(""));
 }
 
-function convertToArray(input) {
-  return input.toString().split("");
+function convertToArray(number) {
+  return number.toString().split("");
 }
 
-function checkIfNumberHasDecimal(input) {
-  return input % 1 == 0 ? input : input.toFixed(4);
+function formatResult(number) {
+  return number % 1 == 0 ? number : parseFloat(number.toFixed(4));
 }
 
-function isThisArrayEmpty(array) {
+function isArrayEmpty(array) {
   return array.length === 0 ? true : false;
 }
